6672f4791edfe2f600130d289eccfedcd9f82497
boris2442 <aubinborissimotsebo@gmail.com>
2025-05-02 17:23:01 +0200

    text

--- participants.php
+++ participants.php
@@ -1,13 +1,20 @@
 <?php
 session_start();
 require_once 'database/database.php';
+if(isset($_SESSION['users']['id']) && $_SESSION['users']['id'] > 0) {
+   //recuperer le nom des participants par evenemenmts
+   
+} else {
+    header('location:event.php');
+    exit();
+}   
 
 // 1. Afficher le titre de la page
 $pageTitle = "Mise à jour du profil";
 // 2. Début du tampon de la page de sortie
 ob_start();
 // 3. Inclure le fichier de configuration ou le fichier de la page d'accueil
-require_once 'layouts/layouts/participant_html.php';
+require_once 'layouts/layouts/participants_html.php';
 // 4. Récupération du contenu du tampon dans la page de sortie
 $pageContent = ob_get_clean();
 // 5. Inclure le layout de la page de sortie
